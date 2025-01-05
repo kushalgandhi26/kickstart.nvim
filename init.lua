bca30cb2903905c1e9889c1989ca999b1001d315
kushalgandhi26 <kushalgandhi2601@gmail.com>
2025-01-06 04:57:49 +0530

    added working directory keymap

--- init.lua
+++ init.lua
@@ -205,6 +205,13 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Custom keymaps
+vim.keymap.set('n', '<leader>pv', ':Vex<CR>', { desc = 'Show directory' })
+vim.keymap.set('n', '<leader>pf', ':Files<CR>', { desc = 'Show directory' })
+vim.keymap.set('v', '<C-f>', ':tmux new-session -d "~/tmux-sessionizer" && tmux attach-session', { desc = 'Tmux' })
+--
+--
+--
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
